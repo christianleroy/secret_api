26e41bc4333ed558a035f36dad8e9a640d39d820
Christian Leroy Castillo <42598080+christianleroy@users.noreply.github.com>
2026-05-10 21:39:03 +0800

    Fix test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,5 @@
 <?php
 
 test('the root route redirects to api documentation', function () {
-    $this->get('/')->assertRedirect('/api/documentation');
+    $this->get('/')->assertRedirect('/api/documentation#/Key%20Values');
 });
